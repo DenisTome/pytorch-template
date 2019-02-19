b386ba0aab84308c9a3032ed640f7252ad12a7bc
denis <D.Tome@cs.ucl.ac.uk>
2019-02-19 18:58:09 +0000

    refactoring train and test

--- test.py
+++ test.py
@@ -1,86 +1,86 @@
 # -*- coding: utf-8 -*-
 """
-Created on Jul 03 16:21 2018
+Test code where everything is specified by the user or
+automatically assigned to the default value
 
 @author: Denis Tome'
-
-Test code where everything is specified by the user or
-automatically assigned to the default value.
 
 """
 import argparse
 import logging
+from torch.utils.data import DataLoader
 from model.model import Model
 from dataset_def import Dataset, ToTensor
 from tester.tester import Tester
 from utils import OUT_DIR
-from torch.utils.data import DataLoader
 
-_logger = logging.getLogger('main')
+_LOGGER = logging.getLogger('main')
 
-parser = argparse.ArgumentParser(description='Tester')
-parser.add_argument(
+PARSER = argparse.ArgumentParser(description='Tester')
+PARSER.add_argument(
     '-b',
     '--batch-size',
     default=64,
     type=int,
     help='mini-batch size (default: 512)')
-parser.add_argument(
+PARSER.add_argument(
     '-n',
     '--name',
     default='train_HMAE',
     type=str,
     help='name of the training (default: train_AE)')
-parser.add_argument(
+PARSER.add_argument(
     '-r',
     '--resume',
     default='',
     type=str,
     help='path to latest checkpoint (default: none)')
-parser.add_argument(
+PARSER.add_argument(
     '--verbosity',
     default=2,
     type=int,
     help='verbosity, 0: quiet, 1: per epoch, 2: complete (default: 2)')
-parser.add_argument(
+PARSER.add_argument(
     '--verbosity_iter',
     default=250,
     type=int,
     help='after how many iterations to show info')
-parser.add_argument(
+PARSER.add_argument(
     '--save-dir',
     default=OUT_DIR,
     type=str,
     help='directory of saved model (default: model/checkpoints)')
-parser.add_argument(
+PARSER.add_argument(
     '--test-path',
     required=True,
     type=str,
     help='file/dir with val-set annotations')
-parser.add_argument(
+PARSER.add_argument(
     '--num-threads',
     default=8,
     type=int,
     help='number of threads for loading data (default: 8)')
-parser.add_argument(
+PARSER.add_argument(
     '-s',
     '--sampling',
     default=5,
     type=int,
     help='sampling used on the test set (default: 5)')
-parser.add_argument(
+PARSER.add_argument(
     '-d',
     '--description',
     default='',
     type=str,
     help='description of the training')
-parser.add_argument(
+PARSER.add_argument(
     '--no-cuda',
     action="store_true",
     help='use CPU in case there\'s no GPU support')
 
 
 def main(args):
+    """Main"""
+
     # Model
     model = Model()
     model.summary()
@@ -115,4 +115,4 @@ def main(args):
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
-    main(parser.parse_args())
+    main(PARSER.parse_args())
--- train.py
+++ train.py
@@ -1,17 +1,16 @@
 # -*- coding: utf-8 -*-
 """
-Created on Jul 03 16:21 2018
+Train code where everything is specified by the user or
+automatically assigned to the default value
 
 @author: Denis Tome'
 
-Train code where everything is specified by the user or
-automatically assigned to the default value.
-
 """
-import os
 import argparse
 import logging
 import torch.optim as optim
+from torch.utils.data import DataLoader
+from torchvision import transforms
 from model.model import Model
 from model.modules.loss import ae_loss as loss
 from model.modules.metric import AvgPoseError
@@ -21,127 +20,127 @@ from dataset_def import Dataset
 from dataset_def import Convert, ToTensor
 from trainer.trainer import Trainer
 from utils import CHKP_DIR
-from torchvision import transforms
-from torch.utils.data import DataLoader
 
-_logger = logging.getLogger('main')
+_LOGGER = logging.getLogger('main')
 
-parser = argparse.ArgumentParser(description='Trainer')
-parser.add_argument(
+PARSER = argparse.ArgumentParser(description='Trainer')
+PARSER.add_argument(
     '-lr',
     '--learning-rate',
     default=1e-4,
     type=float,
     help='learning rate')
-parser.add_argument(
+PARSER.add_argument(
     '-b',
     '--batch-size',
     default=64,
     type=int,
     help='mini-batch size (default: 64)')
-parser.add_argument(
+PARSER.add_argument(
     '-e',
     '--epochs',
     default=100,
     type=int,
     help='number of total epochs (default: 100)')
-parser.add_argument(
+PARSER.add_argument(
     '-n',
     '--training_name',
     default='train',
     type=str,
     help='name of the training (default: train_AE)')
-parser.add_argument(
+PARSER.add_argument(
     '--no-lr-decay',
     action="store_true",
     help='don\'t use learning rate decay')
-parser.add_argument(
+PARSER.add_argument(
     '--lr_decay_rate',
     default=0.95,
     type=float,
     help='learning rate decay rate (default = 0.95)')
-parser.add_argument(
+PARSER.add_argument(
     '--lr_decay_step',
     default=3000,
     type=float,
     help='learning rate decay step (default = 3000)')
-parser.add_argument(
+PARSER.add_argument(
     '-r',
     '--resume',
     default='',
     type=str,
     help='path to latest checkpoint (default: none)')
-parser.add_argument(
+PARSER.add_argument(
     '--verbosity',
     default=2,
     type=int,
     help='verbosity, 0: quiet, 1: per epoch, 2: complete (default: 2)')
-parser.add_argument(
+PARSER.add_argument(
     '--verbosity_iter',
     default=250,
     type=int,
     help='after how many iterations to show info')
-parser.add_argument(
+PARSER.add_argument(
     '--save-dir',
     default=CHKP_DIR,
     type=str,
     help='directory of saved model (default: model/checkpoints)')
-parser.add_argument(
+PARSER.add_argument(
     '--save-freq',
     default=10000,
     type=int,
     help='training checkpoint frequency in number of iterations(default: 10000)')
-parser.add_argument(
+PARSER.add_argument(
     '--img-log-step',
     default=250,
     type=int,
     help='number of iterations for log images (default: 1000)')
-parser.add_argument(
+PARSER.add_argument(
     '--train-log-step',
     default=100,
     type=int,
     help='log frequency in number of iterations for training (default: 500)')
-parser.add_argument(
+PARSER.add_argument(
     '--val-log-step',
     default=250,
     type=int,
     help='log frequency in number of iterations for validation (default: 2000)')
-parser.add_argument(
+PARSER.add_argument(
     '--train-path',
     required=True,
     type=str,
     help='file/directory of training data')
-parser.add_argument(
+PARSER.add_argument(
     '--val-path',
     required=True,
     type=str,
     help='file/directory of validation data')
-parser.add_argument(
+PARSER.add_argument(
     '--eval-epoch',
     action="store_true",
     help='flag to apply input noise during training')
-parser.add_argument(
+PARSER.add_argument(
     '--num-threads',
     default=8,
     type=int,
     help='number of threads for loading data (default: 8)')
-parser.add_argument(
+PARSER.add_argument(
     '-d',
     '--description',
     default='',
     type=str,
     help='description of the training')
-parser.add_argument(
+PARSER.add_argument(
     '--no-cuda',
     action="store_true",
     help='use CPU in case there\'s no GPU support')
-parser.add_argument(
+PARSER.add_argument(
     '--reset',
     action="store_true",
     help='reset global information about previous model')
 
 
 def main(args):
+    """Main"""
+
     # Model
     model = Model()
     model.summary()
@@ -218,4 +217,4 @@ def main(args):
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
-    main(parser.parse_args())
+    main(PARSER.parse_args())
